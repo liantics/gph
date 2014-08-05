681899c8b7f5779a9ea8f51f09c1b6090bee3c5d
Liane Allen <liantics.antics@gmail.com>
2014-08-05 12:27:11 -0400

    Get bootstrap working
    Brought over original home page and installed bootstrap 3. This required creating partials for the home page so I could see what the file was doing (original was over 140 lines).
    
    There are significant changes in behavior between v1 and v3 of
    bootstrap, so layout work will need to be redone, but that will be a
    later task.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ Bundler.require(*Rails.groups)
 
 module GreenPlanetHeroes
   class Application < Rails::Application
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   resources :dashboards, only: [:index, :show, :new, :create]
+  resources :projects, only: [:index]
 
   root "dashboards#index"
 end
