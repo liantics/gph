def481cb35ee0375be502db7cf6b8b37dc77344d
Liane Allen <liantics.antics@gmail.com>
2014-08-19 22:13:40 -0400

    address hound comments

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -3,7 +3,9 @@ class DonationsController < ApplicationController
     @donation = Donation.new(donation_params)
 
     if @donation.save
-      flash[:notice] = "Your donation for #{donation_params[:amount]} is being processed. Thank you #{current_user.name}!"
+      flash[:notice] = "Your donation for #{donation_params[:amount]} is being
+        processed. Thank you #{current_user.name}!"
+
       redirect_to receiving_project
     else
       render root_path
@@ -22,6 +24,6 @@ class DonationsController < ApplicationController
 
   def receiving_project
     level = Level.find(donation_params[:level_id])
-    project = Project.find(level.project_id)
+    Project.find(level.project_id)
   end
 end
