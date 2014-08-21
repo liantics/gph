8baa85f77c7842520f5511bb17a4b0b33365348e
Liane Allen <liantics.antics@gmail.com>
2014-08-20 23:44:23 -0400

    rebase to test projects form css

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -3,24 +3,16 @@ class DonationsController < ApplicationController
     @donation = Donation.new(donation_params)
 
     if @donation.save
-<<<<<<< HEAD
       flash[:notice] = "Your donation for #{@donation.amount} is being
        processed. Thank you #{current_user.name}!"
       redirect_to receiving_project
     else
       redirect_to root_path
-=======
-      flash[:notice] = "Your donation for #{donation_params[:amount]} is being processed. Thank you #{current_user.name}!"
-      redirect_to receiving_project
-    else
-      render root_path
->>>>>>> Add donations to donation levels
     end
   end
 
   private
 
-<<<<<<< HEAD
   def level
     Level.find(params[:level_id])
   end
@@ -33,18 +25,5 @@ class DonationsController < ApplicationController
 
   def receiving_project
     level.project
-=======
-  def donation_params
-    params.require(:donation).permit(
-      :amount,
-      :user_id,
-      :level_id,
-    )
-  end
-
-  def receiving_project
-    level = Level.find(donation_params[:level_id])
-    project = Project.find(level.project_id)
->>>>>>> Add donations to donation levels
   end
 end
