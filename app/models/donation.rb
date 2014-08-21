256429200e5a48c92b29c733b95ef678fc925664
Liane Allen <liantics.antics@gmail.com>
2014-08-20 23:46:28 -0400

    rebasing

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,17 +1,8 @@
 class Donation < ActiveRecord::Base
-<<<<<<< HEAD
   belongs_to :user
   belongs_to :level
 
   validates :user, presence: true
   validates :level, presence: true
   validates :amount, presence: true
-=======
-  validates :user_id, presence: true
-  validates :level_id, presence: true
-  validates :amount, presence: true
-
-  belongs_to :user
-  belongs_to :level
->>>>>>> Add donations to donation levels
 end
