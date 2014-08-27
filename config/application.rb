2a11bc03d490a66acb681375d9dd93a88c79c71f
Liane Allen <liantics.antics@gmail.com>
2014-08-27 04:46:50 -0400

    Explicitly add css file path to precompile list

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module GreenPlanetHeroes
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+  config.assets.precompile += [
+    'custom_css.scss'
+    ]
   end
 end
