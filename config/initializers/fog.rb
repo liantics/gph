61b7d8265851a838c7eeb885e5637a8ec72345b6
Liane Allen <liantics.antics@gmail.com>
2014-09-11 14:10:19 -0400

    respond to pull request comments

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
 
   config.fog_credentials = {
-    provider: 'AWS',
+    provider: "AWS",
     aws_access_key_id: ENV.fetch("AWS_ACCESS_KEY"),
     aws_secret_access_key: ENV.fetch("AWS_SECRET_KEY"),
     path_style: true
