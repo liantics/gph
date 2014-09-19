96bb7e72f8143dc79d4821dbcac43ecb962eada0
Liane Allen <liantics.antics@gmail.com>
2014-09-19 00:13:53 -0400

    fix PR comments

--- app/models/project.rb
+++ app/models/project.rb
@@ -45,9 +45,9 @@ class Project < ActiveRecord::Base
   def sorted_donation_levels
     levels.by_amount
   end
-  
+
   def finish_line_distance
-   100 - percentage_of_goal
+    100 - percentage_of_goal
   end
 
   def near_finish_line?
