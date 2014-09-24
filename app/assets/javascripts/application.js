987182d09d6201a16b29e8543331fe631342a589
Liane Allen <liantics.antics@gmail.com>
2014-09-24 13:06:33 -0400

    Remove green boxes from home page
    Remove boxes from around Your Projects and Projects You've funded.
    Properly align text next to thumbnails in project list.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
 //= require jquery_ujs
+//= require bootstrap
 //= require_tree .
--- lib/generate_column_widths.rb
+++ lib/generate_column_widths.rb
@@ -1,5 +1,9 @@
 module GenerateColumnWidths
-  def generate_widths(new_columns, total_grid_columns: 12, existing_columns: 0, min_column_span: 3, max_column_span: 12)
+  def generate_widths(new_columns,
+                      total_grid_columns: 12,
+                      existing_columns: 0,
+                      min_column_span: 3,
+                      max_column_span: 12)
     columns_in_row = new_columns + existing_columns
     span = (total_grid_columns / columns_in_row).floor
 
