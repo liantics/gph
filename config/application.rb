fa0693b2be8a7f26264aa5935f099a7547322c1c
liantics <liantics.antics@gmail.com>
2016-03-04 20:23:39 -0500

    Centering updates

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module GreenPlanetHeroes
     config.serve_static_assets = true
     config.assets.precompile << "app/assets/custom.css.scss"
     config.autoload_paths += %W(#{config.root}/lib)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
